8d425ab3f8adfd333c2574c2defbaaf06656e449
Saikishore <2043202@cognizant.com>
2022-07-24 12:01:26 +0530

    first commit

---
+++ process-pension-service/src/test/java/com/cts/processPension/ProcessPensionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.processPension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessPensionApplicationTests {
+
+	
+
+}
